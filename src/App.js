fa29769c83c02bcaa4615e3448da8c9928dda978
ackerman <julio.sergio2709@gmail.com>
2022-11-11 15:18:41 -0500

    Creating Router routes from react-router-dom 6.X.X

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faqs";
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
+import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { Home, Browse, Signin, Signup } from './pages';
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer/>
-      <FaqsContainer/>
-      <FooterContainer/>
-    </>
+    <Router>
+      <Routes>  
+        <Route exact path={ROUTES.HOME} element={<Home/>}/>
+        <Route exact path={ROUTES.BROWSE} element={<Browse/>}/>
+        <Route exact path={ROUTES.SIGN_IN} element={<Signin/>}/>
+        <Route exact path={ROUTES.SIGN_UP} element={<Signup/>}/>
+      </Routes>    
+    </Router>
   );
 }
