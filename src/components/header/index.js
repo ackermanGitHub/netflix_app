76c876e6fe49b554ffbe6263e2011604d6ba2963
ackerman <julio.sergio2709@gmail.com>
2022-11-22 21:22:15 -0500

    add: browse, loading and profile page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,19 @@
 /* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
 import React from "react";
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Background, Container, Logo, ButtonLink } from './styles/header';
+import { Background, Container, Logo, ButtonLink, Feature, FeatureCallOut, Text } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? <Background {...restProps}>{children}</Background> : children;
+}
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>
+}
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>
 }
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,11 +1,12 @@
 import React from "react";
-//import selectionFilter from "../utils/selection-map";
+//import selectionFilter from "../utils/selection-filter";
 //import series from '../fixtures/series.json';
 //import films from '../fixtures/films.json';
+import { BrowseContainer } from "../containers/browse";
 
 export default function Browse() {
-
+    //const slides = selectionFilter({ series, films });
     return (
-        <p>Hello from the Browse</p>
+        <BrowseContainer></BrowseContainer>
     )
 }
